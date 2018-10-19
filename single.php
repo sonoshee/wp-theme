9306627e9ec05f21f124bff97cd374c38daa6252
sonoshee <riraku.tsujito@gmail.com>
2018-10-19 09:33:30 +0900

    20181019 single.php

--- single.php
+++ single.php
@@ -1,7 +1,17 @@
 <?php get_header(); ?>
 <div class="container">
   <div class="contents">
-
+    <?php if(have_post()): the_post(); ?>
+    <article <?php post_class('kiji'); ?>>
+      <div class="kiji-info">
+        <span class="kiji-date">
+          <i class="fas fa-pencil-alt"></i>
+          <time>
+          </time>
+        </span>
+      </div>
+    </article>
+    <?php enfif; ?>
   </div>
   <?php get_sidebar(); ?>
 </div>
